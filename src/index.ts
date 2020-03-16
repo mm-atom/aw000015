fda4d4d535230f48b1304b125b00330a01d04cf2
taoqf <tao_qf@163.com>
2020-03-16 10:19:07 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-
-export default async function aw15(param1: string, param2: string) {
+/**
+ * 获取值
+ * @param key 键
+ */
+export default function get<T>(key: string, defualt_value?: T) {
+	const value = localStorage.getItem(key);
+	if (value) {
+		return JSON.parse(value);
+	}
+	return defualt_value;
 }
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,11 @@
 import { expect } from 'chai';
 import t from '../dist/index';
 
-describe('xxx', () => {
-	it('xxx', () => {
-		t();
-		expect(1).not.eq(2);
+describe('storage get', () => {
+	it('get storage value', () => {
+		const key = 'mm';
+		const value = 'studio';
+		const v = t(key, value);
+		expect(v).eq(value);
 	});
 });
